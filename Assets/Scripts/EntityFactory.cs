57f6e776134e9541b19fe40da8ddf00fbe2f07f3
Alper Keskin <alprkskn@gmail.com>
2017-08-01 01:21:46 +0300

    A dummy EntityFactory is added.

--- Assets/Scripts/EntityFactory.cs
+++ Assets/Scripts/EntityFactory.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 namespace RTSDemo
 {
-    public class EntityFactory : Singleton<EntityFactory>
+    /// <summary>
+    /// This factory class is responsible for creation of both 
+    /// Model and Views of the grid entities.
+    /// </summary>
+    public static class EntityFactory
     {
+        // TODO: These methods are placeholders.
+        // These will most probably be changed with 
+        // derived models tomorrow.
 
-        // Use this for initialization
-        void Start()
+        public static BuildingModel CreateBuilding()
         {
-
+            var model = new BuildingModel();
+            ViewFactory.CreateViewForModel<BuildingView>(model);
+            return model;
         }
 
-        // Update is called once per frame
-        void Update()
+        public static UnitModel CreateUnit()
         {
-
+            var model = new UnitModel();
+            ViewFactory.CreateViewForModel<UnitView>(model);
+            return model;
         }
     }
 }
